63f061b4e87ad6b558065568ca98777626d96773
Mgr. Marek Blaha <it@jaroska.cz>
2026-04-21 20:26:40 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,75 @@
+//
+//  main.cpp
+//  supergame
+//
+//  Created by marba on 16.04.2026.
+//
+
+#include <iostream>
+
+using namespace std;
+//* Tým 1 ***************************************************************************
+/**
+ Tato funkce vypise na obrazovku uvodní informace o hre, ve ktere bude hrac hadat cislo od 1 do 1000.
+ Vypiste nazev i pravidla hry do nejakeho pekneho ramecku.
+ */
+void uvodniObrazovka()
+{
+    
+}
+//* Tým 2 ***************************************************************************
+/**
+    Tato funkce vypise hlaseni o ukonceni hry do nejakeho pekneho ramecku a pak pocka na stisk klevesy Enter.
+ */
+void koncovaObrazovka()
+{
+    
+}
+//* Tým 3 ***************************************************************************
+/**
+ Pokud je cisloCloveka vetsi nez cisloPocitace, pak napise na obrazovku "Moje cislo je mensi".
+ Pokud je cisloCloveka mensi nez cisloPocitace, pak napise na obrazovku "Moje cislo je vetsi".
+ Jinak napise na obrazovku "Trefa!".
+  */
+void napisOdpovedPocitace(int cisloCloveka, int cisloPocitace)
+{
+    
+}
+//* Tým 4 ***************************************************************************
+/**
+    Funkce vrati nahodne cele cislo v rozsahu <odkud, kam>.
+ */
+int vymysliNahodneCislo(int odkud, int kam)
+{
+    
+}
+//* Tým 5 ***************************************************************************
+/**
+ Vyzve cloveka k zadani celeho cisla na klavesnici. Toto cele cislo pak funkce vrati jako svuj vysledek.
+ */
+int nactiCisloCloveka()
+{
+    
+}
+//* Tým 6 ***************************************************************************
+/**
+ Pokud se hodnoty cisloCloveka a cisloPocitace rovnaji, pak vraci false.
+ Jinak vraci true.
+ */
+bool hraNekonci(int cisloCloveka, int cisloPocitace)
+{
+    
+}
+//****************************************************************************
+int main() {
+    int cisloCloveka, cisloPocitace;
+    uvodniObrazovka();
+    cisloPocitace = vymysliNahodneCislo(1,1000);
+    do{
+        cisloCloveka=nactiCisloCloveka();
+        napisOdpovedPocitace(cisloCloveka, cisloPocitace);
+    }while(hraNekonci(cisloCloveka, cisloPocitace));
+    
+    koncovaObrazovka();
+    return 0;
+}
